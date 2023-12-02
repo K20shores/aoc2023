d544c2ee63d5f268033c4cb1d9fc682013bb72ab
Kyle Shores <kyle.shores44@gmail.com>
2023-12-02 00:10:53 -0600

    part 2

--- include/aoc2023/trie.hpp
+++ include/aoc2023/trie.hpp
@@ -1,12 +1,71 @@
+#include <stddef.h>
+#include <string>
+#include <queue>
 
 class Trie
 {
     static constexpr size_t ALPHABET_SIZE = 26;
 
-    class Node
+public:
+    struct Node
     {
         Node *children[ALPHABET_SIZE];
-        bool IsTerminal;
-        char value;
+        int value;
+
+        Node() : value(-1)
+        {
+            for (int i = 0; i < ALPHABET_SIZE; ++i)
+            {
+                children[i] = nullptr;
+            }
+        }
     };
+
+    void insert(std::string key, int val)
+    {
+        Node *x = &root;
+        for (const auto &c : key)
+        {
+            size_t idx = c - 97;
+            if (x->children[idx] == nullptr)
+            {
+                x->children[idx] = new Node();
+            }
+            x = x->children[idx];
+        }
+        x->value = val;
+    }
+
+    Node *search(char c) const
+    {
+        if ((c < 97) || (c > 122)) {
+            return nullptr;
+        }
+        return root.children[c - 97];
+    }
+
+    void print()
+    {
+        std::queue<Node *> cue;
+
+        cue.push(&root);
+
+        while (!cue.empty())
+        {
+            Node *x = cue.front();
+            cue.pop();
+            for (const auto &node : x->children)
+            {
+                if (node)
+                {
+                    std::cout << node->value << " ";
+                    cue.push(node);
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+
+private:
+    Node root{};
 };
--- src/day1.cpp
+++ src/day1.cpp
@@ -28,10 +28,58 @@ int part1(std::string line)
 }
 
 // finds the first number represented as 0-9 or zero, one, ..., nine from the left and right, represents them as a two digit number <left><right> and returns that
-int part2(std::string line) {
-    Trie t;
+int part2(std::string line, const Trie &t)
+{
+    int left = -1;
+    int right = -1;
+    Trie::Node *search = nullptr;
 
-    return 0;
+    for (size_t i = 0; i < line.size(); ++i)
+    {
+        char c = line[i];
+        if ((c >= 48) && (c <= 57))
+        {
+            int candidate = int(c) - 48;
+            if (left == -1) {
+                left = candidate * 10;
+            }
+            else {
+                right = candidate;
+            }
+        }
+        else
+        {
+            search = t.search(c);
+            if (search)
+            {
+                for (size_t j = i + 1; j < line.size() && search; ++j)
+                {
+                    char d = line[j];
+                    if ((d < 97) || (d > 122))
+                    {
+                        break;
+                    }
+                    search = search->children[d -97];
+                    if (search && search->value != -1)
+                    {
+                        int candidate = search->value;
+                        if (left == -1) {
+                            left = candidate * 10;
+                        }
+                        else {
+                            right = candidate;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if (right == -1) {
+        right = left / 10;
+    }
+
+    return left + right;
 }
 
 int main()
@@ -40,9 +88,24 @@ int main()
     std::string line;
     int part1_sum{};
     int part2_sum{};
+
+    Trie t;
+    t.insert("zero", 0);
+    t.insert("one", 1);
+    t.insert("two", 2);
+    t.insert("three", 3);
+    t.insert("four", 4);
+    t.insert("five", 5);
+    t.insert("six", 6);
+    t.insert("seven", 7);
+    t.insert("eight", 8);
+    t.insert("nine", 9);
+    t.insert("nine", 9);
+
     while (std::getline(file, line))
     {
         part1_sum += part1(line);
+        part2_sum += part2(line, t);
     }
     std::cout << "Part 1: " << part1_sum << std::endl;
     std::cout << "Part 2: " << part2_sum << std::endl;
