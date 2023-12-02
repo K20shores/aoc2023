acfd5d47d1a011ebf828c0c0c021c0c7e5cfe519
Kyle Shores <kyle.shores44@gmail.com>
2023-12-01 20:36:15 -0600

    day 1 complete

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
 
 int main() {
+    std::ifstream file(std::filesystem::path("inputs/day1.txt"));
+    std::string str; 
+    int sum = 0;
+    while (std::getline(file, str))
+    {
+        auto left = str.begin();
+        auto right = str.rbegin();
+
+        while ((*left < 48) || (*left > 59)) {
+            ++left;
+        }
+        while ((*right < 48) || (*right > 59)) {
+            ++right;
+        }
+        sum += (int(*left) - 48)*10 + (int(*right)-48);
+    }
+    std::cout << sum << std::endl;
 }
