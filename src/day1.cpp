6499823b5ffa9cb2833a9ef37c2c39bc92febfd5
Kyle Shores <kyle.shores44@gmail.com>
2023-12-01 20:37:58 -0600

    adding comments

--- src/day1.cpp
+++ src/day1.cpp
@@ -9,15 +9,20 @@ int main() {
     int sum = 0;
     while (std::getline(file, str))
     {
+        // get forward and backward iterators
         auto left = str.begin();
         auto right = str.rbegin();
 
-        while ((*left < 48) || (*left > 59)) {
+        // advance them until a number is found
+        // take advantage of char's evaluating to strings
+        // 0 is ascii 48, 9 is ascii 57, numbers are anything between these two numbers
+        while ((*left < 48) || (*left > 57)) {
             ++left;
         }
-        while ((*right < 48) || (*right > 59)) {
+        while ((*right < 48) || (*right > 57)) {
             ++right;
         }
+        // subtract 48 from each number to get its value, recourd the sum
         sum += (int(*left) - 48)*10 + (int(*right)-48);
     }
     std::cout << sum << std::endl;
